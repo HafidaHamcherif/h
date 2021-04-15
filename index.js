f703e57a3b226c5b8d335c6163a2db5b84b4ca90
HafidaHamcherif <58478522+HafidaHamcherif@users.noreply.github.com>
2021-04-15 18:59:09 +0200

    new commit

--- index.js
+++ index.js
@@ -28,11 +28,9 @@ mongoose.connect(MONGODB_URI || "mongodb://localhost:27017/Art",{
 });
 
 
-app.get('/',(req, res)=>{
-    res.send('art')
-})
 
-app.get('/art',(req,res) => {
+
+app.get('/',(req,res) => {
     console.log('get/art');
     ArtModel.find({}, (err, art) => {
         if(err !== null) {
